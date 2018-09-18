ab86d449aff54da8c4207161b596706718ef14dd
Love <love.wick93@gmail.com>
2018-09-18 15:25:05 +0200

    Project set up

---
+++ Portfolio_Umbraco_Site/Portfolio_Umbraco_Site.Code/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Portfolio_Umbraco_Site.Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace Portfolio_Umbraco_Site.Code.Controllers
+{
+    public class HomeController : SurfaceController, IRenderMvcController
+    {
+        public ActionResult Index(RenderModel model)
+        {
+            var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+            var umbracoPageId = (int)PageIds.Home;
+            IPublishedContent content = umbracoHelper.TypedContent(umbracoPageId);
+            var lol = content.GetPropertyValue<string>("TestOne");
+            HomeViewModel test = new HomeViewModel
+            {
+                Beeees = "LALLE",
+                Kuuus = "ASDASD",
+                Laaaang = "ÖPPPPP",
+                Jalla = lol
+            };
+
+            return View(test);
+        }
+
+        enum PageIds
+        {
+            Home = 1065,
+        }
+
+    }
+}
---
+++ Portfolio_Umbraco_Site/Portfolio_Umbraco_Site.Code/Models/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace Portfolio_Umbraco_Site.Code.Models
+{
+    public class HomeViewModel : RenderModel
+    {
+        public HomeViewModel() : this(UmbracoContext.Current.PublishedContentRequest.PublishedContent) { }
+
+        public HomeViewModel(IPublishedContent content) : base(content)
+        {
+
+        }
+        public string Kuuus { get; set; }
+        public string Beeees { get; set; }
+        public string Laaaang { get; set; }
+        public string Jalla { get; set; }
+    }
+}
